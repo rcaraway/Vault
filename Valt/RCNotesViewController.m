a46fe50b921a28a2e1b49ebd289e84c28dbb7815
Rob Caraway <home@robcaraway.com>
2014-04-10 12:25:31 -0500

    Notes keyboard launched, failed placeholder

--- Valt/RCNotesViewController.m
+++ Valt/RCNotesViewController.m
@@ -25,30 +25,67 @@
 }
 
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.autofillView.alpha = 0;
+    [self setupTextView];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)infoTapped:(UIButton *)sender {
+
+#pragma mark - Actions
+
+- (IBAction)infoTapped:(UIButton *)sender
+{
+
 }
 
-- (IBAction)autoFillTapped:(UIButton *)sender {
+- (IBAction)autoFillTapped:(UIButton *)sender
+{
+
 }
+
+
+#pragma mark - TextView
+
+-(void)setupTextView
+{
+    self.textView.delegate = self;
+    [self.textView becomeFirstResponder];
+    self.textView.text = @"Foobar placeholder";
+    self.textView.textColor = [UIColor lightGrayColor];
+    self.textView.tag = 0;
+}
+
+
+-(void)textViewDidChange:(UITextView *)textView
+{
+    if (textView.text.length > 0){
+        if(textView.tag == 0) {
+            textView.text = @"";
+            textView.textColor = [UIColor blackColor];
+            textView.tag = 1;
+        }
+    }
+}
+
+
+-(void)textViewDidEndEditing:(UITextView *)textView
+{
+    if([textView.text length] == 0)
+    {
+        textView.text = @"Foobar placeholder";
+        textView.textColor = [UIColor lightGrayColor];
+        textView.tag = 0;
+    }
+}
+
+
+
+
 @end
